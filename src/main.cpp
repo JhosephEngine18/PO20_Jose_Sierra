ed5ee8c68b92814339c4caa53bd80f32f8c69a72
DevSierra <josesielo1805@gmail.com>
2025-03-25 10:53:42 -0600

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+/*
+Universidad Amerike
+Author: José Sierra
+Project: Task List
+Date: 25/03/25
+Description: This program will let you type personal data of 3 persons
+*/
+
+#include <iostream>
+using namespace std;
+int Selection;
+int setter;
+
+void Separations()
+{
+    cout << "--------------------------------";
+}
+
+void Options()
+{
+    Separations();
+    cout << "1 =  Add";
+    Separations();
+    cout << "2 =  Delete";
+    Separations();
+    cout << "4 =  Watch Tasks";
+    Separations();
+    cout << "5 =  Leave";
+    switch (Selection)
+    {
+    case 1:
+        
+        if (condition)
+        {
+            /* code */
+        }
+        
+        
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void AddTask()
+{
+
+}
+
+void ListofTask()
+{
+
+}
+
+int main()
+{
+    Separations();
+    cout << "What would you like to do?";
+    Options();
+
+}
